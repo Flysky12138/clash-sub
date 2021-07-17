5f1cdcf3460b16edc37729afe3e4a0b515a21471
Flysky12138 <2797299614@qq.com>
2021-07-17 15:27:41 +0800

    0717

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # clash-sub
 
-使用：https://clash-sub.vercel.app/?url=https://aa.com&ruleType=0&filter='香港,新加坡'&host=ltetp.tv189.com
+```
+yarn
+node index
+```
+
+使用：http://127.0.0.1:666/?url=https://aa.com&ruleType=0&filter='香港,新加坡'&host=ltetp.tv189.com
 
 - `url` Clash 订阅地址
 
--- index.js
+++ index.js
@@ -9,23 +9,28 @@ const express = require('express')
 const app = express()
 app.get('/', function (request, response) {
   const queryMap = getUrlQueryMap(decodeURI(request.url))
-  getSubArray(queryMap.get('url'))
-    .then(res => {
-      // 转换文件
-      const result = mixin({
-        subArr: res,
-        ruleType: queryMap.has('ruleType') ? queryMap.get('ruleType') : 1,
-        filter: queryMap.has('filter') ? queryMap.get('filter') : '.',
-        host: queryMap.has('host') ? queryMap.get('host') : 'ltetp.tv189.com'
+  if (queryMap.has('url')) {
+    getSubArray(queryMap.get('url'))
+      .then(res => {
+        // 转换文件
+        const result = mixin({
+          subArr: res,
+          ruleType: queryMap.has('ruleType') ? queryMap.get('ruleType') : 1,
+          filter: queryMap.has('filter') ? queryMap.get('filter') : '.',
+          host: queryMap.has('host') ? queryMap.get('host') : 'ltetp.tv189.com'
+        })
+        // 保存文件
+        fs.writeFileSync(
+          './config.yaml',
+          YAML.stringify(result, null, '  ').replace(/(dns:\n  |proxies:\n  -|proxy-groups:\n  -|rule-providers:\n  |rules:\n  -)/gi, '\n$1')
+        )
+        response.download('config.yaml')
       })
-      // 保存文件
-      fs.writeFileSync(
-        './config.yaml',
-        YAML.stringify(result, null, '  ').replace(/(dns:\n  |proxies:\n  -|proxy-groups:\n  -|rule-providers:\n  |rules:\n  -)/gi, '\n$1')
-      )
-      response.download('config.yaml')
-    })
-    .catch(console.log)
+      .catch(console.log)
+  } else {
+    response.write(request.url)
+    response.end()
+  }
 })
 // 监听端口
 app.listen(666, function () {
