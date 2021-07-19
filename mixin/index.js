8a8b721180261a1e9557917b9c8fa2d94fd06601
Flysky12138 <2797299614@qq.com>
2021-07-19 18:45:36 +0800

    0719 - 节点过滤改成正则匹配

--- mixin/index.js
+++ mixin/index.js
@@ -8,7 +8,7 @@ const rules = require('./rules')
 // 订阅节点数组，代理规则，需要的节点名
 function mixin(obj) {
   // 过滤得到需要的节点名
-  const namelists = obj.subArr.map(res => res.ps).filter(res => new RegExp(`/${obj.filter}/`).test(res))
+  const namelists = obj.subArr.map(res => res.ps).filter(res => new RegExp(obj.filter).test(res))
 
   return {
     ...header,
