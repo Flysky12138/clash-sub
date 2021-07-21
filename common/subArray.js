6b9b0e9c74374cf5f54ab12e238be07a46000d89
Flysky12138 <2797299614@qq.com>
2021-07-21 10:24:21 +0800

    0721

--- common/subArray.js
+++ common/subArray.js
@@ -1,36 +1,34 @@
 const axios = require('axios')
 const atob = require('./atob')
 
-// 单个订阅请求
-async function once(url) {
-  return await axios({
-    url,
-    method: 'get',
-    timeout: 5000
-  })
-}
-
 // 获取订阅内容，返回包含节点对象的数组
 async function subArray(url, add) {
-  let [funcHttp, vmessArrayStr] = [[], [...add.split(',')]]
-  // 创建包含请求方法的数组
-  url.split(',').forEach(element => funcHttp.push(once(element)))
-  // 多订阅同时请求
-  return await axios.all(funcHttp).then(res => {
-    // vmess节点链接放入vmessArrayStr数组中
-    res.forEach(element => {
-      atob(element.data)
-        .replace(/\s/g, ',')
-        .split(',')
-        .forEach(element => vmessArrayStr.push(element))
-    })
-    // 将vmess节点链接转换成JSON对象
-    const vmessArrayObj = vmessArrayStr.filter(item => item.length !== 0).map(item => JSON.parse(atob(item.replace('vmess://', '')), null, '  '))
-    // 过滤重复名
-    let reslut = []
-    vmessArrayObj.forEach(element => reslut.findIndex(item => item.ps === element.ps) === -1 && reslut.push(element))
-    return reslut
+  // 追加vmess节点
+  let vmessArrayStr = [...add.split(',')]
+  // 订阅请求
+  let data = []
+  for (const i of url.split(',')) {
+    data.push(
+      await axios({
+        url: i,
+        method: 'get',
+        timeout: 5000
+      }).then(res => res.data)
+    )
+  }
+  // vmess节点放入vmessArrayStr数组中
+  data.forEach(element => {
+    atob(element)
+      .replace(/\s/g, ',')
+      .split(',')
+      .forEach(_element => vmessArrayStr.push(_element))
   })
+  // 将vmess节点链接转换成JSON对象
+  const vmessArrayObj = vmessArrayStr.filter(item => item.length !== 0).map(item => JSON.parse(atob(item.replace('vmess://', '')), null, '  '))
+  // 过滤重复名
+  let reslut = []
+  vmessArrayObj.forEach(element => reslut.findIndex(item => item.ps === element.ps) === -1 && reslut.push(element))
+  return reslut
 }
 
 module.exports = subArray
